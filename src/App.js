2b91d0a5c6764ca82bc54eef3f56aa993e55a2ca
wjddngus1023 <wjddngus102@naver.com>
2022-01-28 11:10:30 +0900

    added processenv on path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element= {<Home/>}>
+        <Route path={`${process.env.PUBLIC_URL}/`} element= {<Home/>}>
         </Route>
         <Route path = "/movie/:id" element = {<Detail/>}>
         </Route>
